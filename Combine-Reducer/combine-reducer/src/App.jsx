823cc86cb5e2c7e47feb6b681db841157f84e760
Aditya7j <89588001+Aditya7j@users.noreply.github.com>
2022-03-28 18:08:49 +0530

    combine
    combine

---
+++ Combine-Reducer/combine-reducer/src/App.jsx
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Login } from './components/Login';
+import { Navbar } from './components/Navbar';
+import { Home } from './components/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Routes>
+        <Route path='/' element= {<Home/>}></Route>
+        <Route path='/login' element= {<Login/>}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
